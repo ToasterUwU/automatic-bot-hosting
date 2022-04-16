a349ae470dc42af6b952be1c408cc71463293950
ToasterUwU <toasterger@gmail.com>
2022-04-16 16:16:26 +0200

    wait for Popen to finisch, fix order of stuff

--- cogs/bot_setup.py
+++ cogs/bot_setup.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-import zipfile
+import sys
 
 import nextcord
 from config import *
@@ -27,38 +27,72 @@ class Example(commands.Cog):
             return await ctx.reply("No attachment found")
 
         try:
-            with open(self.BOT_ROOT_PATH + "/temp.zip") as f:
-                ctx.message.attachments[0].save(f)
+            await ctx.message.attachments[0].save(self.BOT_ROOT_PATH + "/temp.zip")
         except:
             return await ctx.reply("Error while saving zip file")
 
         try:
             os.mkdir(f"{self.BOT_ROOT_PATH}/{name}")
+        except FileExistsError:
+            pass
         except:
             return await ctx.reply("Error while making directory")
 
         try:
-            with zipfile.ZipFile(self.BOT_ROOT_PATH + "/temp.zip", "r") as zip_ref:
-                zip_ref.extractall(f"{self.BOT_ROOT_PATH}/{name}")
+            subprocess.Popen(
+                f"unzip -o -q {self.BOT_ROOT_PATH}/temp.zip -d {self.BOT_ROOT_PATH}/{name}",
+                shell=True,
+            ).communicate()
         except:
             return await ctx.reply("Error while extracting zip file")
 
         try:
-            service = self.SERVICE_PATH.format(name=name, BOT_ROOT_PATH=self.BOT_ROOT_PATH)
+            service = self.SERVICE_TEMPLATE.format(
+                name=name, BOT_ROOT_PATH=self.BOT_ROOT_PATH
+            )
             with open(f"{self.SERVICE_PATH}/discord-bot-{name}.service", "w+") as f:
                 f.write(service)
         except:
             return await ctx.reply("Error while saving service file")
 
         try:
-            subprocess.Popen(f"systemctl enable discord-bot-{name}.service", shell=True)
+            subprocess.Popen(
+                f"python3 -m venv {self.BOT_ROOT_PATH}/{name}/.venv",
+                shell=True,
+                stderr=sys.stderr,
+            ).communicate()
+        except:
+            return await ctx.reply("Error while making venv.")
+
+        try:
+            subprocess.Popen(
+                f"{self.BOT_ROOT_PATH}/{name}/.venv/bin/pip3 install -U -r {self.BOT_ROOT_PATH}/{name}/requirements.txt",
+                shell=True,
+                stderr=sys.stderr,
+            ).communicate()
+        except:
+            return await ctx.reply("Error while getting dependencies.")
+
+        try:
+            subprocess.Popen(
+                f"systemctl enable discord-bot-{name}.service",
+                shell=True,
+                stderr=sys.stderr,
+            ).communicate()
         except:
             return await ctx.reply("Error while enabling service file")
 
         try:
-            subprocess.Popen(f"systemctl start discord-bot-{name}.service", shell=True)
+            subprocess.Popen(
+                f"systemctl restart discord-bot-{name}.service",
+                shell=True,
+                stderr=sys.stderr,
+            ).communicate()
         except:
             return await ctx.reply("Error while starting service")
+
+        await ctx.reply("Bot set up and running.")
+
 
 def setup(bot):
     bot.add_cog(Example(bot))
