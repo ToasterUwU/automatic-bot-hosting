9207eb6c22d1dc83e7e22aa8fd84d365106bee87
ToasterUwU <toasterger@gmail.com>
2023-02-07 00:17:00 +0100

    clone from github setup

--- cogs/bot_setup.py
+++ cogs/bot_setup.py
@@ -1,12 +1,16 @@
+import asyncio
 import os
 import subprocess
 import sys
+from typing import Optional
 
+import aiohttp
 import nextcord
-from nextcord.ext import commands
+from git.repo import Repo
+from nextcord.ext import commands, tasks
 
 from config import *
-from internal_tools.configuration import CONFIG
+from internal_tools.configuration import CONFIG, JsonDictSaver
 from internal_tools.discord import *
 
 
@@ -16,8 +20,12 @@ class BotSetup(commands.Cog):
         self.SERVICE_PATH = "/lib/systemd/system"
         self.BOT_ROOT_PATH = "/home/aki"
 
-        with open("SERVICE_TEMPLATE.service") as f:
+        with open("SERVICE_TEMPLATE.service", "r") as f:
             self.SERVICE_TEMPLATE = f.read()
+
+        self.repodirs_to_update = JsonDictSaver("repodirs_to_update")
+
+        self.update_repos.start()
 
     async def cog_application_command_check(self, interaction: nextcord.Interaction):
         """
@@ -32,6 +40,14 @@ class BotSetup(commands.Cog):
             return await self.bot.is_owner(user)
         else:
             return False
+
+    async def log(self, message: str):
+        if CONFIG["GENERAL"]["ERROR_WEBHOOK_URL"]:
+            async with aiohttp.ClientSession() as session:
+                webhook = nextcord.Webhook.from_url(
+                    CONFIG["GENERAL"]["ERROR_WEBHOOK_URL"], session=session
+                )
+                await webhook.send(message)
 
     def setup_new_bot(self, name: str):
         try:
@@ -81,6 +97,16 @@ class BotSetup(commands.Cog):
 
         return "Bot set up and running."
 
+    @tasks.loop(minutes=10)
+    async def update_repos(self):
+        for repo_dir, should_update in self.repodirs_to_update.items():
+            if should_update:
+                fetch_info = Repo(repo_dir).remote().pull()
+                if len(fetch_info) > 0:
+                    await self.log(
+                        f"`UPDATED:` {repo_dir} with `{len(fetch_info)}` changes.\n{'\n'.join([str(x) for x in fetch_info])}"
+                    )
+
     @nextcord.slash_command(
         name="manual-setup",
         description="Uses a source_code.zip to setup Bot. This requires manual updates.",
@@ -117,7 +143,16 @@ class BotSetup(commands.Cog):
             await interaction.send("Error while extracting zip file")
             return
 
-        await interaction.send(self.setup_new_bot(name))
+        try:
+            os.remove(f"{self.BOT_ROOT_PATH}/temp.zip")
+        except:
+            await interaction.send("Error while deleting temp zip file")
+            return
+
+        end_message = await asyncio.get_running_loop().run_in_executor(None, self.setup_new_bot, name)
+        await interaction.send(end_message)
+
+        await self.log(f"`SETUP:` {self.BOT_ROOT_PATH}/{name} in `MANUAL` mode")
 
     @nextcord.slash_command(
         name="github-setup",
@@ -127,7 +162,77 @@ class BotSetup(commands.Cog):
     async def github_setup(
         self, interaction: nextcord.Interaction, github_repo_name: str
     ):
-        pass
+        await interaction.response.defer()
+
+        if "/" not in github_repo_name:
+            github_repo_name = "ToasterUwU/" + github_repo_name
+
+        if github_repo_name.count("/") > 1:
+            await interaction.send(
+                "More than one / found, something aint right with that repo name."
+            )
+            return
+
+        name = github_repo_name.split("/")[1]
+
+        try:
+            Repo.clone_from(
+                f"ssh://git@github.com:{github_repo_name}.git",
+                f"{self.BOT_ROOT_PATH}/{name}",
+            )
+        except:
+            await interaction.send(
+                f"Error while cloning repo from git@github.com:{github_repo_name}.git"
+            )
+            return
+
+        self.repodirs_to_update[f"{self.BOT_ROOT_PATH}/{name}"] = True
+        self.repodirs_to_update.save()
+
+        end_message = await asyncio.get_running_loop().run_in_executor(None, self.setup_new_bot, name)
+        await interaction.send(end_message)
+
+        await self.log(f"`SETUP:` {self.BOT_ROOT_PATH}/{name} in `AUTOMATIC` mode")
+
+    @nextcord.slash_command(
+        name="toggle-auto-update",
+        description="Toggles auto pull from master brach for a repo.",
+        guild_ids=CONFIG["GENERAL"]["OWNER_COG_GUILD_IDS"],
+    )
+    async def toggle_auto_update(self, interaction: nextcord.Interaction, name: str):
+        repo_dir = f"{self.BOT_ROOT_PATH}/" + name
+        if repo_dir not in self.repodirs_to_update:
+            await interaction.send("This repo isnt setup yet.")
+            return
+
+        self.repodirs_to_update[repo_dir] = not self.repodirs_to_update[repo_dir]
+        self.repodirs_to_update.save()
+
+        await interaction.send(
+            f"Update status for {repo_dir} = {self.repodirs_to_update[repo_dir]}"
+        )
+
+    @toggle_auto_update.on_autocomplete("name")
+    async def autocomplete_bot_name(
+        self, interaction: nextcord.Interaction, name: Optional[str]
+    ):
+        if name == None:
+            await interaction.response.send_autocomplete(
+                [
+                    x.replace(f"{self.BOT_ROOT_PATH}/", "")
+                    for x in self.repodirs_to_update.keys()
+                ][:20]
+            )
+        else:
+            await interaction.response.send_autocomplete(
+                [
+                    x.replace(f"{self.BOT_ROOT_PATH}/", "")
+                    for x in self.repodirs_to_update.keys()
+                    if x.replace(f"{self.BOT_ROOT_PATH}/", "")
+                    .casefold()
+                    .startswith(name.casefold())
+                ][:20]
+            )
 
 
 def setup(bot):
